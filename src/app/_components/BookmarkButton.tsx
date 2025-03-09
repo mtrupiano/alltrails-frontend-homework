2ea80eeaa8185ab71f34ab70d6b329bb11f351a8
Mark Trupiano <markt6749@gmail.com>
2025-03-08 23:33:56 -0800

    correct event type for button onClick event

--- src/app/_components/BookmarkButton.tsx
+++ src/app/_components/BookmarkButton.tsx
@@ -4,7 +4,7 @@ import BookmarkRestingSvg from "@/assets/icons/bookmark-resting.svg";
 
 export default function BookmarkButton({ enabled }: { enabled: boolean }) {
   const [isEnabled, setIsEnabled] = useState(enabled);
-  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     console.log("clicked");
     event.stopPropagation();
     setIsEnabled(!isEnabled);
