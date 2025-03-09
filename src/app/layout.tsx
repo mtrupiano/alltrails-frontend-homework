bfbdbab9daec509ddee2a20c36a22b382585a171
Mark Trupiano <markt6749@gmail.com>
2025-03-08 18:37:09 -0800

    figure out theme fonts; add shades of green-house

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Manrope } from "next/font/google";
 import "./globals.css";
+import { Manrope } from "next/font/google";
 
-const manrope500 = Manrope({ weight: "500" });
+const manrope = Manrope({
+  variable: "--font-manrope",
+});
 
 export const metadata: Metadata = {
   title: "AllTrails at lunch",
@@ -15,10 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={manrope500.className + " min-w-[375px]"}>
-        {children}
-      </body>
+    <html lang="en" className={manrope.variable}>
+      <body className="min-w-[375px]">{children}</body>
     </html>
   );
 }
